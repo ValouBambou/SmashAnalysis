239c0d0968a49bf1790d3f9cbee3bd25dddace42
Valentin Trophime <theval358@gmail.com>
2022-01-25 19:03:42 +0100

    win rates are computed once and reused after

--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,10 +1,38 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
-
 object Example {
+  def compute_win_rates(spark: SparkSession, sc: SparkContext, data_file_name:String): Map[(String, String), Double] = {
+
+    // Load local file data
+    val df = spark.read.format("csv").option("header", "true")
+      .load("resources/" + data_file_name)
+      .select("Winner Character", "Loser Character")
+    df.show()
+
+    val wins_count = df.rdd
+      .map(row => ((row(0).toString, row(1).toString), 1))
+      .reduceByKey((acc, count) => acc + count)
+      .collect()
+      .toMap
+
+    println(wins_count)
+
+    val win_rates = wins_count.map(it => {
+      val winner = it._1._1
+      val looser = it._1._2
+      val num_wins = it._2.toDouble
+      val total_match = num_wins + wins_count.getOrElse((looser, winner), 0).toDouble
+      (it._1, num_wins / total_match)
+    })
+    println(win_rates)
+
+    sc.parallelize(win_rates.toSeq).saveAsTextFile("resources/win_rates")
+    win_rates
+  }
+
   def main(args: Array[String]): Unit = {
-    var conf = new SparkConf().setAppName("Read CSV File").setMaster("local[*]")
+    val conf = new SparkConf().setAppName("Read CSV File").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val spark = SparkSession
       .builder()
@@ -12,18 +40,15 @@ object Example {
       .config("spark.some.config.option", "some-value")
       .getOrCreate()
 
-    // Load local file data
-    val df = spark.read.format("csv").option("header", "true")
-      .load("resources/sample_data.csv")
-      .select("Stage Name", "Winner Character", "Loser Character")
-    df.show()
-
-
-    val character_to_int = sc.textFile("resources/characters.csv").collect()
-    val n_characters = character_to_int.length
-
-    val wins_matrix = Array.ofDim[Int](n_characters, n_characters)
-    wins_matrix.foreach(it => println(it.mkString("[", ", ", "]")))
-
+    var win_rates = Map.empty[(String, String), Double]
+    try {
+      win_rates = sc.textFile("resources/win_rates").map(s => {
+        val tmp = s.replace("(", "").replace(")", "").split(",")
+        ((tmp(0), tmp(1)), tmp(2).toDouble)
+      }).collect().toMap
+    } catch {
+      case e: Exception => win_rates = compute_win_rates(spark, sc, "sample_data.csv")
+    }
+    println(win_rates)
   }
 }
