7be81a1941a01631b8fee35313a45720bd6a489e
Valentin Trophime <theval358@gmail.com>
2022-01-25 17:18:30 +0100

    add spark sql lib now read scv with spark

--- build.sbt
+++ build.sbt
@@ -6,5 +6,6 @@ scalaVersion := "2.13.8"
 
 libraryDependencies++=Seq(
   "org.scala-lang"%"scala-library"%"2.13.8",
-  "org.apache.spark"%"spark-core_2.13"%"3.2.0"
+  "org.apache.spark"%"spark-core_2.13"%"3.2.0",
+  "org.apache.spark"%"spark-sql_2.13"%"3.2.0"
 )
--- src/main/scala/Example.scala
+++ src/main/scala/Example.scala
@@ -1,12 +1,20 @@
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+
 
 object Example {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Spark Job for Loading Data").setMaster("local[*]") // local[*] will access all core of your machine
-    val sc = new SparkContext(conf) // Create Spark Context
+    var conf = new SparkConf().setAppName("Read CSV File").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+
     // Load local file data
-    val emp_data = sc.textFile("ressources/example_data.csv") // It will return a RDD
-    // Read the records
-    println(emp_data.foreach(println))
+    val df = spark.read.format("csv").option("header", "true").load("resources/sample_data.csv")
+    df.printSchema()
+    df.show()
   }
 }
